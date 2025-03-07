55bcd41fc469d5157112db7f38fccdcad52bb5ec
Barsa-Rani <nayakbarsarani2003@gmail.com>
2025-03-07 12:27:22 +0530

    Update PsaApplication.java
    Second Commit

--- src/main/java/in/psa/PsaApplication.java
+++ src/main/java/in/psa/PsaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PsaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(PsaApplication.class, args);
 	}
 
